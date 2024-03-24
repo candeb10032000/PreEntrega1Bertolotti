de5ae63688d88e8a5cf82473cd23e57e095d3aba
Candela Bertolotti <bertolotticandela@gmail.com>
2024-03-24 15:41:09 -0300

    modificacion de carrito

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-// Bucle do-while
+// Primer Entrega
+/*
 
 let usuario = "candela"
 let contraseña = "bertolotti123"
@@ -7,8 +8,8 @@ do {
     let usuarioIngresado = prompt ("Ingrese su usuario");
     let contraseñaIngresada = prompt ("Ingrese su contraseña");
     
-    if (usuarioIngresado === usuario && contraseñaIngresada === contraseña) {
-        alert ("Bienvenida "+ usuario + " a MALALA SHOES");
+    if (usuarioIngresado === usuario && contraseñaIngresada === contraseña ) {
+        alert ("Bienvenida "+ usuario + " a Malala shoes");
         break;
     } else{
         alert ("Usuario y/o contraseña incorrectas"
@@ -17,16 +18,15 @@ do {
 } while (true);
 
 
-// if-else
 
-let compraOnline = prompt("Desea realizar una compra? (si/no)");
+
+let compraOnline = prompt("Desea realizar una compra en MALALA SHOES? (si/no)");
 if (compraOnline === "si") {
     alert("Iniciemos tu compra");
 } else {
     alert("Vuelva pronto, lo esperamos!!")
 } 
 
- // switch 
 
 let listaProductos = prompt("Selecciona un producto:\n1- Zapatos\n2- Borcegos\n3- Zapatillas")
 switch (listaProductos) {
@@ -45,33 +45,83 @@ switch (listaProductos) {
 
 }
 
-let talle = parseInt(prompt("Ingrese su talle:"));
-if (talle >=35 && talle <=45){
-    alert ("Tenemos disponibilidad de ese talle. Realice su compra!")
-}else {
+let talle = prompt("Ingrese su talle:");
+if (talle >= 35 && talle <= 45) {
+    alert("Tenemos disponibilidad de ese talle. Realice su compra!")
+} else {
     alert("Lo siento, no tenemos disponibilidad en ese talle.");
 }
 
-// function  
 
-let totalCarrito = 0;
+*/
 
-function totalCompra(n1, n2) {
-    totalCarrito = n1 * n2;
-    return totalCarrito;
+// Segunda entrega 
+
+function bienvenidos() {
+    alert("Bienvenida a Malala Shoes")
+};
+
+bienvenidos()
+
+const productos = [
+    { nombre: "borcegos", precio: 25000 },
+    { nombre: "zapatos", precio: 22000 },
+    { nombre: "zapatillas", precio: 18000 },
+];
+
+let carrito = []
+
+let seleccionDelUsuario = prompt("¿Deseas realizar una compra? si/no")
+
+while (seleccionDelUsuario != "si" && seleccionDelUsuario != "no") {
+    alert("Por favor ingresa si o no")
+    seleccionDelUsuario = prompt("Deseas realizar una compra en Malala shoes?")
 }
 
-function calcularMontoFinal(cantidadPares, precioPorPar) {
-    let total = totalCompra(cantidadPares, precioPorPar);
-    return total;
+if (seleccionDelUsuario == "si") {
+    alert("A continuación nuestra lista de productos");
+    let MisProductos = productos.map(producto => producto.nombre + " " + producto.precio + "$");
+    alert(MisProductos.join(" - "));
+} else if (seleccionDelUsuario == "no") {
+    alert("¡Gracias por visitarnos, hasta pronto!");
 }
 
-let cantidadPares = prompt("¿Cuántos pares deseas comprar?");
-let precioPorPar = 12000; 
+while (seleccionDelUsuario != "no") {
+    let producto = prompt("Agrega un producto a tu carrito")
+    let precio = 0
 
-if (cantidadPares) {
-    let montoFinal = calcularMontoFinal(Number(cantidadPares), precioPorPar);
-    alert("El monto total de tu compra es: $" + montoFinal);
-} else {
-    alert("Ingresa un número válido para la cantidad de productos");
+    if (producto == "borcegos" || producto == "zapatos" || producto == "zapatillas") {
+        switch (producto) {
+            case "borcegos":
+                precio = 25000;
+                break;
+            case "zapatos":
+                precio = 22000;
+                break;
+            case "zapatillas":
+                precio = 18000;
+                break;
+            default:
+                break;
+        }
+        let unidades = parseInt(prompt("Cuantas unidades de este producto comprar?"))
+
+        carrito.push({ producto, unidades, precio })
+        console.log(carrito)
+    }
+    else {
+        alert("No tenemos stock de este producto")
+    }
+    seleccionDelUsuario = prompt("Desea seguir con la compra?")
+
+    while (seleccionDelUsuario === "no") {
+        alert("Gracias por su compra.")
+        carrito.forEach((carritoFinal) => {
+            console.log(`producto: ${carritoFinal.producto}, unidades; ${carritoFinal.unidades} , total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+        break;
+    }
 }
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+console.log(`El total a pagar de su compra es: ${total}`)
---
+++ js/productosclass.js
@@ -0,0 +1,10 @@
+/*
+class producto{
+    constructor (nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+console.log (producto)
+*/
